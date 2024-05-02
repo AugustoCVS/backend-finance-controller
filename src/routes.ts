5aba5360162bb314b4b39cd40cc34aad65450664
AugustoCVS <guty.vertolis@gmail.com>
2024-05-02 18:13:16 -0300

    feat: add login route

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { CreateUserController } from "./useCases/createUser/CreateUserController";
+import { AuthenticateUserController } from "./useCases/authenticateUser/AuthenticateUserController";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
+const authenticateUserController = new AuthenticateUserController();
 
-router.post("/users", createUserController.handle)
+router.post("/register", createUserController.createUser)
+router.post("/login", authenticateUserController.login)
 
 export { router };	
