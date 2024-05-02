834a35c0c5398a4abbbd03286878ce6c9bcceb75
AugustoCVS <guty.vertolis@gmail.com>
2024-05-02 17:44:42 -0300

    feat: create error middleware

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import "express-async-errors"
+import express, {Request, Response, NextFunction} from "express";
 import { router } from "./routes";
 
 const app = express();
@@ -6,5 +7,18 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  if (error instanceof Error) {
+    return response.status(400).json({
+      error: error.message
+    });
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  });
+})
 
 app.listen(3001, () => console.log("Rodando na porta 3001"));
