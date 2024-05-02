e2ded274202686588c68a08ffb0daffa95d3fe7a
AugustoCVS <guty.vertolis@gmail.com>
2024-05-02 18:13:01 -0300

    refactor: change handle name

--- src/useCases/createUser/CreateUserController.ts
+++ src/useCases/createUser/CreateUserController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
 class CreateUserController {
-  async handle(request: Request, response: Response) {
+  async createUser(request: Request, response: Response) {
     const { name, email, password, confirm_password } = request.body;
 
     const authenticateUserUseCase = new CreateUserUseCase();
