45a85dfb20a435972085b5ce07b9f5b21380b8d9
AugustoCVS <guty.vertolis@gmail.com>
2024-05-02 17:37:27 -0300

    feat: create server

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { router } from "./routes";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(router);
+
+app.listen(3001, () => console.log("Rodando na porta 3001"));
