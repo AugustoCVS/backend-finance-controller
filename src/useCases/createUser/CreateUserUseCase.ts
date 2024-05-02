9f7ffcaa2ba7c8c3675325ca16f39d3052df1088
AugustoCVS <guty.vertolis@gmail.com>
2024-05-02 17:38:33 -0300

    feat: create create user useCase

---
+++ src/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,44 @@
+import { hash } from "bcryptjs";
+
+import { client } from "../../prisma/client";
+
+interface IUserRequest {
+  name: string;
+  email: string;
+  password: string;
+  confirm_password: string;
+}
+
+class CreateUserUseCase {
+  async execute({ confirm_password, email, name, password }: IUserRequest) {
+    if (password !== confirm_password) {
+      throw new Error("Senhas não conferem");
+    }
+
+    const userAlreadyExists = await client.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("Usuário ja existe");
+    }
+
+    const passwordHash = await hash(password, 8);
+    const confirm_passwordHash = await hash(confirm_password, 8);
+
+    const user = await client.user.create({
+      data: {
+        name,
+        email,
+        password: passwordHash,
+        confirm_password: confirm_passwordHash,
+      },
+    });
+
+    return user;
+  }
+}
+
+export { CreateUserUseCase };
