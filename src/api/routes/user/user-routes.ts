b3fc617730dbd4474ff1084bbb0a7dc935d94d98
AugustoCVS <guty.vertolis@gmail.com>
2024-05-03 03:54:48 -0300

    feat: create refreshToken route

--- src/api/routes/user/user-routes.ts
+++ src/api/routes/user/user-routes.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
 
 import { CreateUserController } from "../../controllers/user/user.controller";
-import { AuthenticateUserController } from "../../controllers/auth/auth-controller";
+import { AuthenticateUserController } from "../../controllers/auth/auth.controller";
+import { RefreshTokenUserController } from "../../controllers/auth/refresh-token.controller";
 
 const userRoutes = Router();
 
 const createUserController = new CreateUserController();
 const authenticateUserController = new AuthenticateUserController();
+const refreshTokenController = new RefreshTokenUserController();
 
 userRoutes.post("/register", createUserController.createUser)
 userRoutes.post("/login", authenticateUserController.login)
+userRoutes.post("/refreshToken", refreshTokenController.handle)
 
 export { userRoutes };	
