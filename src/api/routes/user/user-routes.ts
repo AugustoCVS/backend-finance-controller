af74ed5d152999c19dabd57a04bce556a2240e68
AugustoCVS <guty.vertolis@gmail.com>
2024-05-08 11:01:21 -0300

    feat: create route for getUser

--- src/api/routes/user/user-routes.ts
+++ src/api/routes/user/user-routes.ts
@@ -1,19 +1,21 @@
 import { Router } from "express";
 
-import { CreateUserController } from "../../controllers/user/user.controller";
+import { UserController } from "../../controllers/user/user.controller";
 import { AuthenticateUserController } from "../../controllers/auth/auth.controller";
 import { RefreshTokenUserController } from "../../controllers/auth/refresh-token.controller";
 import { ensureAuthenticated } from "../../../middlewares/ensure-authenticated";
 
 const userRoutes = Router();
 
-const createUserController = new CreateUserController();
+const userController = new UserController();
 const authenticateUserController = new AuthenticateUserController();
 const refreshTokenController = new RefreshTokenUserController();
 
-userRoutes.post("/register", createUserController.createUser)
+userRoutes.post("/register", userController.createUser)
 userRoutes.post("/login", authenticateUserController.login)
 userRoutes.post("/refreshToken", refreshTokenController.handle)
+userRoutes.get("/:id", ensureAuthenticated, userController.getUser)
+
 userRoutes.get("/profile", ensureAuthenticated, (req, res) => {
   return res.json({ message: "Hello World" });
 })
