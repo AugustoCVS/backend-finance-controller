626afcd7f153fe8434b1b3f7674ebf56bea5e805
AugustoCVS <guty.vertolis@gmail.com>
2024-05-08 14:56:41 -0300

    feat: create delete account service

---
+++ src/services/use-cases/account/delete-account.service.ts
@@ -0,0 +1,23 @@
+import { client } from "../../../infra/prisma/client";
+
+class DeleteAccountService {
+  async execute({ id }: { id: string }): Promise<void> {
+    try {
+      const account = await client.account.findUnique({
+        where: { id },
+      });
+
+      if (!account) {
+        throw new Error("Conta não encontrada");
+      }
+
+      await client.account.delete({
+        where: { id },
+      });
+    } catch (error) {
+      throw new Error("Falha ao deletar conta: " + error.message);
+    }
+  }
+}
+
+export { DeleteAccountService };
