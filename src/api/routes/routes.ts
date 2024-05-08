75708b1dd00fa25b4d83f9f245deb1de61d7e451
AugustoCVS <guty.vertolis@gmail.com>
2024-05-08 14:26:12 -0300

    feat: add accounts routes

--- src/api/routes/routes.ts
+++ src/api/routes/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { userRoutes } from "./user/user-routes";
+import { accountRoutes } from "./account/account-routes";
 
 const router = Router();
 
 router.use("/users", userRoutes);
+router.use("/accounts", accountRoutes);
 
 export { router };
