3a1e1888b530b796a771d87eb233e43302f83e6b
AugustoCVS <guty.vertolis@gmail.com>
2024-05-08 11:29:43 -0300

    feat: create updateUser on UserController

--- src/api/controllers/user/user.controller.ts
+++ src/api/controllers/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { CreateUserService } from "../../../services/use-cases/user/create-user.service";
 import { GetUserService } from "../../../services/use-cases/user/get-user.service";
+import { UpdateUserService } from "../../../services/use-cases/user/update-user.service";
 class UserController {
   async createUser(request: Request, response: Response) {
     const { name, email, password, confirm_password } = request.body;
@@ -17,13 +18,27 @@ class UserController {
     return response.json(user);
   }
 
-  async getUser(request: Request, response: Response) {
+  async getUserById(request: Request, response: Response) {
     const { id } = request.params;
 
     const getUserUseCase = new GetUserService();
 
     const user = await getUserUseCase.execute({
       userId: id,
+    });
+
+    return response.json(user);
+  }
+
+  async updateUser(request: Request, response: Response) {
+    const { id } = request.params;
+    const { name, email } = request.body;
+
+    const updateUserUseCase = new UpdateUserService();
+
+    const user = await updateUserUseCase.execute(id, {
+      name,
+      email,
     });
 
     return response.json(user);
