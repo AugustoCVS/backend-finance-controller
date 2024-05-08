7a5c213d9a1646975343240b7ee062510e1a3826
AugustoCVS <guty.vertolis@gmail.com>
2024-05-08 14:57:36 -0300

    feat: create get, update and delete account routes

--- src/api/routes/account/account-routes.ts
+++ src/api/routes/account/account-routes.ts
@@ -7,5 +7,8 @@ const accountRoutes = Router();
 const accountController = new AccountController();
 
 accountRoutes.post("/create", ensureAuthenticated, accountController.createAccount);
+accountRoutes.get("/list/:userId", ensureAuthenticated, accountController.getAccounts);
+accountRoutes.put("/update/:id", ensureAuthenticated, accountController.updateAccount);
+accountRoutes.delete("/delete/:id", ensureAuthenticated, accountController.deleteAccount);
 
 export { accountRoutes };
