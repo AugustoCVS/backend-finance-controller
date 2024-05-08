77775d3a42cb1399ee489e59c73f38781eef42bb
AugustoCVS <guty.vertolis@gmail.com>
2024-05-08 18:00:17 -0300

    feat: add transactions routes

--- src/api/routes/routes.ts
+++ src/api/routes/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { userRoutes } from "./user/user-routes";
 import { accountRoutes } from "./account/account-routes";
+import { transactionsRoutes } from "./transactions/transactions-routes";
 
 const router = Router();
 
 router.use("/users", userRoutes);
 router.use("/accounts", accountRoutes);
+router.use("/transactions", transactionsRoutes);
 
 export { router };
