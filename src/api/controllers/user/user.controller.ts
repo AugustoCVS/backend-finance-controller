9ee746f4bab5c399233c74c24938a4c65a05507f
AugustoCVS <guty.vertolis@gmail.com>
2024-05-08 11:48:53 -0300

    feat: create delete user on UserController

--- src/api/controllers/user/user.controller.ts
+++ src/api/controllers/user/user.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { CreateUserService } from "../../../services/use-cases/user/create-user.service";
 import { GetUserService } from "../../../services/use-cases/user/get-user.service";
 import { UpdateUserService } from "../../../services/use-cases/user/update-user.service";
+import { DeleteUserService } from "../../../services/use-cases/user/delete-user.service";
 class UserController {
   async createUser(request: Request, response: Response) {
     const { name, email, password, confirm_password } = request.body;
@@ -42,6 +43,18 @@ class UserController {
     });
 
     return response.json(user);
+  }
+
+  async deleteUser(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const deleteUserUseCase = new DeleteUserService();
+
+    await deleteUserUseCase.execute({
+      userId: id,
+    });
+
+    return response.status(204).send();
   }
 }
 
